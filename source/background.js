c15992360189466e67cf878a7211380162f93a38
Wesley Moore <wes@wezm.net>
2021-07-09 19:06:48 +1000

    Initial implementation

---
+++ source/background.js
@@ -0,0 +1,11 @@
+"use strict";
+browser.webRequest.onBeforeSendHeaders.addListener(function (e) {
+    if (e.requestHeaders) {
+        for (var header of e.requestHeaders) {
+            if (header.name.toLowerCase() === "user-agent") {
+                header.value = (header.value || "").replace(/Linux/g, "Mac OS X");
+            }
+        }
+    }
+    return { requestHeaders: e.requestHeaders };
+}, { urls: ["https://*.github.com/*"] }, ["blocking", "requestHeaders"]);
